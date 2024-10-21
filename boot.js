9579f4fef242356744eccb7e968970a6da7a6811
Raghu M <raghulucky@gmail.com>
2024-10-21 13:41:13 +0530

    first commit

---
+++ boot.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('.slidebar').slick({
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+    });
+
+});
+
+$(document).ready(function(){
+    $('.slide').slick({
+        
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+          
+    });
+  });
